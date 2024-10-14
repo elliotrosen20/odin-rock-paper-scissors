aa88473d9c0c109e83b8568a542b341044dc33a6
Elliot Rosen <elliotrosen2@gmail.com>
2024-10-14 14:00:23 -0400

    Remove redundant commented functions

--- script.js
+++ script.js
@@ -8,54 +8,6 @@ function getHumanChoice() {
     return choice.toLowerCase();
 }
 
-// let humanScore = 0;
-// let computerScore = 0;
-
-// function playRound(humanChoice, computerChoice) {
-//     if (humanChoice == "rock" && computerChoice == "scissors") {
-//         humanScore++;
-//         console.log(`You win, ${humanChoice} beats ${computerChoice}!`);
-//     } else if (humanChoice == "scissors" && computerChoice == "paper") {
-//         humanScore++;
-//         console.log(`You win, ${humanChoice} beats ${computerChoice}!`);
-//     } else if (humanChoice == "paper" && computerChoice == "rock"){
-//         humanScore++;
-//         console.log(`You win, ${humanChoice} beats ${computerChoice}!`);
-//     } else if (computerChoice == "rock" && humanChoice == "scissors") {
-//         computerScore++;
-//         console.log(`You lose, ${computerChoice} beats ${humanChoice}.`);
-//     } else if (computerChoice == "scissors" && humanChoice == "paper") {
-//         computerScore++;
-//         console.log(`You lose, ${computerChoice} beats ${humanChoice}.`);
-//     } else if (computerChoice == "paper" && humanChoice == "rock"){
-//         computerScore++;
-//         console.log(`You lose, ${computerChoice} beats ${humanChoice}.`);
-//     } else if (humanChoice == computerChoice) {
-//         console.log("You tied!")
-//     }
-// }
-
-// function playRound(humanChoice, computerChoice) {
-//     if ((humanChoice == "rock" && computerChoice == "scissors") || (humanChoice == "scissors" && computerChoice == "paper") || (humanChoice == "paper" && computerChoice == "rock")){
-//         humanScore++;
-//         console.log(`You win, ${humanChoice} beats ${computerChoice}!`);
-//     } else if (humanChoice == computerChoice) {
-//         console.log("You tied!");
-//     } else {
-//         computerScore++;
-//         console.log(`You lose, ${computerChoice} beats ${humanChoice}.`);
-//     }
-// }
-
-// const humanSelection = getHumanChoice();
-// console.log(humanSelection)
-// const computerSelection = getComputerChoice();
-// console.log(computerSelection)
-
-// playRound(humanSelection, computerSelection);
-// console.log(humanScore);
-// console.log(computerScore);
-
 function playGame() {
     let humanScore = 0;
     let computerScore = 0;
